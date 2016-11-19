dab090e07ca2267b955913b4ea46a7bb61b00927
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2016-11-19 15:50:50 +0100

    veranderingen direct zichtbaar maken

--- app/src/main/java/nl/xs4all/pebbe/batdreamer/MyDaydreamService.java
+++ app/src/main/java/nl/xs4all/pebbe/batdreamer/MyDaydreamService.java
@@ -22,6 +22,7 @@ public class MyDaydreamService extends DreamService {
     private float density;
     private TimerTask timerTask;
     private ConstraintLayout layout;
+    private ImageView image;
     private int imageID;
     private Canvas canvas;
     private Context context;
@@ -72,7 +73,7 @@ public class MyDaydreamService extends DreamService {
 
         layout = (ConstraintLayout) findViewById(R.id.daydreamLayout);
         imageID = R.id.imageView;
-        ImageView image = (ImageView) findViewById(imageID);
+        image = (ImageView) findViewById(imageID);
 
         float size = 92 * density;
         Bitmap bitmap = Bitmap.createBitmap((int)(2 * size +.5), (int)(2 * size + .5), Bitmap.Config.ARGB_8888);
@@ -82,8 +83,6 @@ public class MyDaydreamService extends DreamService {
 
         context = getApplicationContext();
         ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-
-        paint();
     }
 
     @Override
@@ -108,6 +107,8 @@ public class MyDaydreamService extends DreamService {
 
     private void paint() {
 
+        //verplaatsen
+
         float xp = rnd.nextFloat();
         float yp = rnd.nextFloat();
         ConstraintSet c = new ConstraintSet();
@@ -116,11 +117,13 @@ public class MyDaydreamService extends DreamService {
         c.setVerticalBias(imageID, yp);
         c.applyTo(layout);
 
+        // waardes opvragen
+
         Intent batteryStatus = context.registerReceiver(null, ifilter);
-        float battery = 0;
+        float battery = .01f;
         try {
-            int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-            int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+            int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, 1);
+            int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, 100);
             battery = level / (float) scale;
         } catch (Exception e) {
             // ignore
@@ -133,6 +136,8 @@ public class MyDaydreamService extends DreamService {
         }
         int minute = now.get(Calendar.MINUTE);
         String time = String.format("%d:%02d", hour, minute);
+
+        // tekenen
 
         canvas.drawARGB(255, 0, 0, 0);
 
@@ -157,5 +162,9 @@ public class MyDaydreamService extends DreamService {
         p.setStyle(Paint.Style.FILL);
         p.setTextSize(density * 26);
         canvas.drawText(time, -.5f * p.measureText(time), 8 * density, p);
+
+        // maak veranderingen zichtbaar
+
+        image.invalidate();
     }
 }
